3207e824ca482505cc2a5470205e21b9619375b0
ckdash-git <mail.ckdash@gmail.com>
2024-07-23 14:49:01 +0530

    Initial Commit

---
+++ Uber/App/UberApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberApp.swift
+//  Uber
+//
+//  Created by Chandan Kumar Dash on 23/07/24.
+//
+
+import SwiftUI
+
+@main
+struct UberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Uber/Core/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Uber
+//
+//  Created by Chandan Kumar Dash on 23/07/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
