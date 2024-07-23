0f972e2f65ec94bcea07ab956e1e0b462999cd7b
ckdash-git <mail.ckdash@gmail.com>
2024-07-23 16:03:35 +0530

    Precise location zooming

--- Uber/Managers/LocationManager.swift
+++ Uber/Managers/LocationManager.swift
@@ -5,4 +5,23 @@
 //  Created by Chandan Kumar Dash on 23/07/24.
 //
 
-import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject {
+    private let locationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard !locations.isEmpty else { return }
+        locationManager.stopUpdatingLocation()
+    }
+}
