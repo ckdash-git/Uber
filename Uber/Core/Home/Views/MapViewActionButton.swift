df76c52f1f24a6f24b303b5a3f54b867cc833b40
ckdash-git <mail.ckdash@gmail.com>
2024-07-23 16:41:13 +0530

    Map added with "Where to go" search bar.

---
+++ Uber/Core/Home/Views/MapViewActionButton.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewActionButton.swift
+//  Uber
+//
+//  Created by Chandan Kumar Dash on 23/07/24.
+//
+
+import SwiftUI
+
+struct MapViewActionButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapViewActionButton()
+}
---
+++ Uber/Core/LocationSearch/Views/LocationSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationSearchView.swift
+//  Uber
+//
+//  Created by Chandan Kumar Dash on 23/07/24.
+//
+
+import SwiftUI
+
+struct LocationSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationSearchView()
+}
