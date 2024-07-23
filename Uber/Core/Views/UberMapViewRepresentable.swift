b35c1417cb6b56e3a8d15a2fbc6b319476089542
ckdash-git <mail.ckdash@gmail.com>
2024-07-23 15:08:30 +0530

    First commit

--- Uber/Core/Views/UberMapViewRepresentable.swift
+++ Uber/Core/Views/UberMapViewRepresentable.swift
@@ -5,4 +5,38 @@
 //  Created by Chandan Kumar Dash on 23/07/24.
 //
 
-import Foundation
+import SwiftUI
+import MapKit
+
+struct UberMapViewRepresentable: UIViewRepresentable {
+    
+    let mapView = MKMapView()
+    
+    func makeUIView(context: Context) -> some UIView {
+        mapView.isRotateEnabled = false
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+        
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> MapCoordinator {
+        return MapCoordinator(parent: self)
+    }
+}
+
+extension UberMapViewRepresentable {
+    
+    class MapCoordinator: NSObject, MKMapViewDelegate {
+        let parent: UberMapViewRepresentable
+        
+        init(parent: UberMapViewRepresentable){
+            self.parent = parent
+            super.init()
+        }
+    }
+}
