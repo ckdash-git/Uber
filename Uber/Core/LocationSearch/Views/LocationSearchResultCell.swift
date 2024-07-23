181c33ebc9988735854bb9991dcce4fd97eb9f7a
ckdash-git <mail.ckdash@gmail.com>
2024-07-23 17:53:46 +0530

    Added Current location & destination textfield.

---
+++ Uber/Core/LocationSearch/Views/LocationSearchResultCell.swift
@@ -0,0 +1,18 @@
+//
+//  LocationSearchResultCell.swift
+//  Uber
+//
+//  Created by Chandan Kumar Dash on 23/07/24.
+//
+
+import SwiftUI
+
+struct LocationSearchResultCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationSearchResultCell()
+}
